edce0fb632e118354dea05514b20d15713e72edc
Laura Borbolla <butyoucancallmecharlie@gmail.com>
2018-03-26 17:59:51 -0600

    Primeros avances de AHR.
    Vale, está la primera parte del AHR. Funciona la prueba que está
    ahí y usa la base de datos. Subo esta versión para guardar cambios,
    pues todavía faltan varias cosas por hacer.
    
    Aunque me surgieron varias preguntas... por ejemplo, en nuestra
    basecilla, los PAN y token tienen el atributo de ser únicos; pero
    este algoritmo puede tener varios tokens para un mismo PAN, así que
    se va al garete la parte de único. Y, ¿no tiene eso sentido? Porque,
    se supone que estamos dando servicio a varios establecimientos, ¿no?
    Supongamos que hay un sujeto que compra en dos tiendas a las que
    hay que darles tokens, ¿les vamos a dar el mismo token aunque sea
    la misma tarjeta? El PAN no debe ser único, ¿o sí?
    
    Bueno, la otra es que, por ahora, esta cosa acepta por separaddo
    la entrada X (lo que se va a tokenizar) y la entrada U (la adicional,
    que creo que nosotros tomaremos como el IIN). Ahora, voy a cambiar
    eso para que la entrada sea el PAN solamente, así puedo separar yo
    el IIN tomando los seis primeros dígitos y lo que se va a tokenizar:
    los siguientes dígitos sin tomar el último.
    Ohh, eso me recuerda que tengo otra pregunta, cuando guarde el token,
    
    ¿guardo solo lo que calculé (9 dígitos si el PAN es de 16)
    o guardo el nuevo token (pegándole el IIN y calculando el nuevo
    dígito verificador)?
    
    AALSDJF esa línea a la mitad de la nada es por tonto Vi.
    Pensé que había cambiado al editor por defecto.

---
+++ implementaciones/ahr/ahr.cpp
@@ -0,0 +1,256 @@
+#include "cabeceras/ahr.hh"
+#include "../aes_ensamblador/cabeceras/aes.hh"
+#include "../../utilidades/cabeceras/arreglo_de_digitos.hh"
+#include "../acceso_a_datos/cabeceras/registro.hh"
+#include <cryptopp/sha.h>
+#include <cryptopp/hex.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace CryptoPP;
+using namespace Implementaciones;
+using namespace std;
+
+/**
+ * Constructor por defecto. Se encarga de inicializar los bloques bloqueT y
+ * bloqueC. El único parámetro que recibe un entero N que indica el Número
+ * de bits necesarios para poder almacenar la entradaX.
+ */
+AHR::AHR(int longitudToken, CDV* baseDeDatos)
+: N{longitudToken}, accesoADatos{baseDeDatos}
+{
+  bloqueT = new unsigned char[M];
+  bloqueC = new unsigned char[M];
+}
+
+/**
+ * Se encarga de crear un objeto a partir de
+ * otro ya existente. Ambos objetos quedan utilizables.
+ */
+AHR::AHR(AHR const& otro)
+{
+  bloqueT = new unsigned char[M];
+  memcpy(bloqueT, otro.bloqueT, M);
+
+  bloqueC = new unsigned char[M];
+  memcpy(bloqueC, otro.bloqueC, M);
+
+  entradaX = otro.entradaX;
+  entradaU = otro.entradaU;
+
+  accesoADatos = otro.accesoADatos;
+}
+
+/**
+ * Caso especial del operador de asignación donde
+ * la fuente y el destino son del mismo tipo. A diferencia del constructor
+ * por copia, limpia los miembros antes de asignarlos.
+ */
+AHR& AHR::operator=(AHR const& otro)
+{
+  unsigned char *auxiliar1 = new unsigned char[M];
+  memcpy(auxiliar1, otro.bloqueT, M);
+  delete [] bloqueT;
+  bloqueT = auxiliar1;
+
+  unsigned char *auxiliar2 = new unsigned char[M];
+  memcpy(auxiliar2, otro.bloqueC, M);
+  delete [] bloqueC;
+  bloqueC = auxiliar2;
+
+  entradaX = otro.entradaX;
+  entradaU = otro.entradaU;
+
+  accesoADatos = otro.accesoADatos;
+
+  return *this;
+}
+
+/**
+ * Se encarga de liberar la memoria reservada por los constructores.
+ */
+AHR::~AHR()
+{
+  delete [] bloqueT;
+  delete [] bloqueC;
+}
+
+/**
+  * Este método se encarga de poner en los últimos N bytes del bloqueT
+  * la entradaX en binario. Utiliza una máscara que inicia con un uno en la
+  * posición más significativa y va haciendo un corrimiento hacia la derecha,
+  * comparando con la entradaX; si encuentra un uno en la posición
+  * correspondiente, mascaraByte se encarga de poner un uno en el bit j del
+  * byte i correspondiente.
+  */
+void AHR::obtenerBitsX()
+{
+  memset(bloqueT, 0x00, M);
+  unsigned long long int mascara =
+    (unsigned long long int) pow((long double)2, (long double)(8*N)-1);
+  unsigned char mascaraByte;
+
+  for(int i = M-N; i < M; i++)
+  {
+    mascaraByte = 0x80;
+    for(int j = 0; j < 8; j++)
+    {
+      if(mascara & entradaX)
+      {
+        bloqueT[i] = bloqueT[i]|mascaraByte;
+      }
+      mascaraByte = mascaraByte >> 1;
+      mascara = mascara >> 1;
+    }
+  }
+}
+
+/**
+  * Este método se encarga del primer paso del algoritmo: obtener el bloqueT
+  * que será utilizado como entrada para el cifrador por bloques. Primero
+  * obtiene la salida de la función pública (SHA256 en este caso) de la entrada
+  * adicional entradaU. Luego llama a la función obtenerBitsX para llenar los
+  * últimos N bloques del bloqueT y, finalmente, le concatena al inicio los
+  * bytes más significativos de la salida del SHA para completar el tamaño
+  * de bloque del cifrador.
+  */
+void AHR::crearBloqueT()
+{
+  string cadenaU  = to_string(entradaU);
+  SHA256 funcionF;
+  byte salidaFCompleta[SHA256::DIGESTSIZE];
+
+  /* Obtener valor SHA256 de la entrada adicional**/
+  funcionF.Update(
+    reinterpret_cast<const unsigned char*>(cadenaU.c_str()),
+    cadenaU.length()
+  );
+  funcionF.Final(salidaFCompleta);
+
+  obtenerBitsX();
+
+  int j = 0;
+  for(int i=0; i < M - N; i++)
+  {
+    bloqueT[i] = salidaFCompleta[j];
+    j++;
+  }
+}
+
+/**
+ * Se encarga de obtener la representación decimal de los últimos N bits del
+ * bloqueC. Guarda el resultado en el entero token.
+ */
+void AHR::obtenerNumeroC()
+{
+  unsigned char mascara;
+  unsigned long long int auxiliar;
+  unsigned long long int auxiliar2 = (8*N)-1;
+  token = 0;
+
+  for(int i = M-N; i < M; i++)
+  {
+    mascara = 0x80;
+    for(int j = 8; j > 0; j--)
+    {
+      if(mascara & bloqueC[i])
+      {
+        auxiliar =
+          (unsigned long long int) pow((long double)2, (long double)auxiliar2);
+        token += auxiliar;
+      }
+      mascara = mascara >> 1;
+      auxiliar2 -= 1;
+    }
+  }
+}
+
+/**
+ * Se encarga de revisar si el token creado existe en la base de datos.
+ */
+bool AHR::existeToken()
+{
+  ArregloDeDigitos token_arreglo = ArregloDeDigitos(this->token);
+  Registro busqueda = accesoADatos->buscarPorToken(token_arreglo);
+  
+  if (busqueda.obtenerToken() != Arreglo<int>{})
+  {
+    /* El token creado ya existe en la base de datos.
+     * Se regresa verdadero para que el algoritmo cree un nuevo token.
+     */
+    return true;
+  }
+  else
+  {
+    /* El token creado no existe en la base de datos.
+     * Se guarda la relación token - pan en la base de datos.
+     */
+    ArregloDeDigitos pan_arreglo = ArregloDeDigitos(this->entradaX);
+    busqueda.colocarToken(token_arreglo);
+    busqueda.colocarPAN(pan_arreglo);
+    accesoADatos->guardar(busqueda);
+    return false;
+  }
+}
+
+/**
+ * Método principal que se encarga de crear el token. Primero inicializa las
+ * entradas entradaX y entradaU, y pone la llave a utilizar en el cifrador.
+ * Luego comienza con el algoritmo:
+ * 1. Crea el bloqueT al concatenar la salida truncada del SHA256 (alimentada
+ *    con entradaU) y la representación binaria de entradaX.
+ * 2. Realiza al menos una vez los siguientes pasos:
+ *  2.1 Cifrar el bloqueT mediante AES256 y lo guarda en el bloqueC.
+ *  2.2 Obtiene la representación decimal de los últimos N bits del bloqueC
+ *      y los guarda en token.
+ *  2.3 Comprueba que el valor en token sea menor a 10^L: en otras palabras,
+ *      verifica que tenga L dígitos. En caso de que no lo tenga, usa el método
+ *      de la caminata cíclica al regresar al paso 2.1, pero cifrando el bloqueC
+ *      actual.
+ * 3. Verifica si existe el token en la base de datos. Si existe, regresa al
+ *    paso 1, con la misma llave y la misma entrada entradaX, pero aumenta
+ *    en uno la entrada entradaU.
+ */
+void AHR::tokenizarHibridamente(unsigned char* llave,
+  unsigned long int entradaX, unsigned long int entradaU)
+{
+  this->entradaX = entradaX;
+  this->entradaU = entradaU;
+
+  AES cifrador = AES(AES_256);
+  cifrador.ponerLlave(llave);
+
+  unsigned long long int limiteS =
+    (unsigned long long int) pow((long double)10, (long double)L);
+
+  /* Primer paso del algoritmo. */
+  crearBloqueT();
+
+  /* Caminata cíclica para obtener un token dentro del dominio del mensaje. */
+  do{
+    /* Segundo paso del algoritmo: cifrar el bloqueT y ponerlo en bloqueC. */
+    cifrador.cifrarBloque(bloqueT);
+    bloqueC = cifrador.obtenerBloqueTCifrado();
+
+    /* Tercer y cuarto paso del algoritmo: verificar que esté dentro del dominio
+     * del mensaje y pasarlo a su forma decimal.
+     */
+    obtenerNumeroC();
+    memcpy(bloqueT, bloqueC, M);
+  }while(token >= limiteS);
+
+  /* Quinto paso del algoritmo: revisar si existe el token generado.*/
+  if(existeToken())
+  {
+    tokenizarHibridamente(llave, entradaX, entradaU+1);
+  }
+}
+
+/**
+ * Se encarga de regresar el valor almacenado en la variable token.
+ */
+unsigned long long int AHR::obtenerToken()
+{
+  return token;
+}
---
+++ implementaciones/ahr/cabeceras/ahr.hh
@@ -0,0 +1,74 @@
+#ifndef __AHRHH__
+#define __AHRHH__
+
+#include "../../acceso_a_datos/cabeceras/cdv.hh"
+#include "../../acceso_a_datos/cabeceras/acceso_mysql.hh"
+
+/** \brief  Tamaño del bloque del cifrador en bytes: 16 bytes = 256 bits*/
+#define M 16
+
+/** \brief  Número de digitos de la entrada X:  */
+#define L 16
+
+namespace Implementaciones
+{
+  class AHR{
+  private:
+    /** \brief  Entero con la entrada a tokenizar. */
+    unsigned long long int entradaX;
+
+    /** \brief  Entero con la entrada adicional (tweak). */
+    unsigned long long int entradaU;
+
+    /** \brief  Entero que almacena el token generado. */
+    unsigned long long int token;
+
+    /** \brief  Bloque que concatena la salida del SHA256 y la entradaX en binario. */
+    unsigned char *bloqueT;
+
+    /** \brief  Bloque que contiene al bloque cifrado. */
+    unsigned char *bloqueC;
+
+    /** \brief  Número de bytes necesarios para almacenar la entradaX:
+      * N = log2(10^L) <- en bits, hay que pasarlo a bytes.
+      */
+    int N;
+
+    /** \brief Pasa a binario entradaX y la almacena en los últimos N bits de bloqueT.*/
+    void obtenerBitsX();
+
+    /** \brief Concatena en T la salida truncada del SHA y la entradaX en binario.*/
+    void crearBloqueT();
+
+    /** \brief Guarda en token los últimos N bits de bloqueC en base 10. */
+    void obtenerNumeroC();
+
+    /** \brief  Verifica si existen el token creado en la base de datos. */
+    bool existeToken();
+
+    /** \brief Apuntador a la clase de acceso a la base de datos. */
+    CDV* accesoADatos;
+
+  public:
+    /** \brief Constructor: recibe el número de bits para guardar a entradaX en base 2.*/
+    AHR(int, CDV*);
+
+    /** C\brief Constructor por copia. */
+    AHR(AHR const&);
+
+    /** \brief Operador de asignación. */
+    AHR& operator= (AHR const&);
+
+    /** \brief Destructor.*/
+    ~AHR();
+
+    /** \brief Método que obtiene el token dada una llave, una entradaX y una entradaU.*/
+    void tokenizarHibridamente(unsigned char*, unsigned long int,
+      unsigned long int);
+
+    /** \brief Método que permite acceder a la variable token.*/
+    unsigned long long int obtenerToken();
+  };
+}
+
+#endif
