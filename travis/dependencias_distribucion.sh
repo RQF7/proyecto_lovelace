a7f19f2749f7d8402aa4d4e2b65d6022d9962db4
Ricardo Quezada <qf7.ricardo@gmail.com>
2018-04-07 21:55:04 -0500

    Actualización de documentación y modificaciones a script de travis
    * Borré el archivo de prueba_RSA.cpp: está lleno de cosas que hacen
      ruido en la documentación; no está integrado a la estructura general
      de pruebas; está en spanglish.
    * Pasé CBC al espacio de nombres de Implementaciones. Espero no haber
      matado alguna otra cosa; las pruebas automatizadas corren.
    * Cambié el flujo del script de travis: las dependencias de la
      documentación y archivos generados (latex y doxygen) no se instalan
      sino hasta después de que el programa pasó las pruebas. Esto tendría
      que acelerar el proceso; cuando menos en casos de error. Para esto
      creé un script de shell que se llama desde el travis, para ramificar
      todo un poco más.
    * La versión de la documentación en línea se veía un poco distinta que
      mi versión local (siempre veo mi versión local, por lo que no me había
      dado cuenta); supongo que es por el relativo retraso de la versión del
      doxygen de ubuntu. Ahora lo estoy instalando desde la página oficial.

---
+++ travis/dependencias_distribucion.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+#
+# Instalación de dependencias de distribución.
+# LaTeX y doxygen
+#
+# Proyecto Lovelace.
+#
+
+# Desde repositorios de ubuntu:
+sudo apt-get -qq install \
+  \ texlive
+  \ texlive-latex-recommended
+  \ texlive-latex-extra
+  \ texlive-lang-spanish
+  \ texlive-bibtex-extra
+  \ texlive-generic-extra
+  \ graphviz
+
+# glossaries
+wget --quiet http://mirrors.ctan.org/install/macros/latex/contrib/glossaries.tds.zip
+unzip --qq -d ~/texmf glossaries.tds.zip
+rm glossaries.tds.zip
+
+# tracklang
+wget --quiet http://mirrors.ctan.org/install/macros/generic/tracklang.tds.zip
+unzip -qq -d ~/texmf tracklang.tds.zip
+rm tracklang.tds.zip
+
+# bibtex
+wget --quiet https://iweb.dl.sourceforge.net/project/biblatex/biblatex-3.9/biblatex-3.9.tds.tgz
+tar -xf biblatex-3.9.tds.tgz -C ~/texmf
+rm biblatex-3.9.tds.tgz
+
+# babel
+wget --quiet http://mirrors.ctan.org/install/macros/latex/required/babel-base.tds.zip
+unzip -qq -d ~/texmf babel-base.tds.zip
+rm babel-base.tds.zip
+
+# biber
+wget --quiet https://phoenixnap.dl.sourceforge.net/project/biblatex-biber/biblatex-biber/current/binaries/Linux/biber-linux_x86_64.tar.gz
+mkdir ~/ejecutable_biber
+tar -xf biber-linux_x86_64.tar.gz -C ~/ejecutable_biber
+export PATH=~/ejecutable_biber/:$PATH
+rm biber-linux_x86_64.tar.gz
+
+# doxygen
+wget --quiet http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.14.linux.bin.tar.gz
+mkdir ~/ejecutable_doxygen
+tar -xf doxygen-1.8.14.linux.bin.tar.gz -C ~/ejecutable_doxygen
+export PATH=~/ejecutable_doxygen/bin:$PATH
+rm doxygen-1.8.14.linux.bin.tar.gz
+
+
