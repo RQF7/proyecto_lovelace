70ea83daa38d9cff6c58d9c31496ff7560af6541
Ricardo Quezada <qf7.ricardo@gmail.com>
2018-04-09 09:19:20 -0500

    UML de clases de generador pseudoaleatorio
    Aquí va el diagrama de clases del DRBG (junto con modificaciones menores a los
    demás). También puse una breve explicación y separé el texto del diseño
    en varios archivos.

--- implementaciones/drbg/cabeceras/ctr_drbg.hh
+++ implementaciones/drbg/cabeceras/ctr_drbg.hh
@@ -66,7 +66,7 @@ namespace Implementaciones
       /** \brief Longitud de la llave interna. */
       unsigned int mLongitudLlave;
 
-      /** \breif Longitud de bloque. */
+      /** \brief Longitud de bloque. */
       unsigned int mLongitudBloque;
 
       /** \brief Longitud de contador. */
