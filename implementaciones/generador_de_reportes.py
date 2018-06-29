aec51aed5c57d8769d47409c481a2e1de6d39472
Ricardo Quezada <qf7.ricardo@gmail.com>
2018-06-29 16:17:05 -0500

    Actualización de toma de reportes
    Me quede generando la gráfica de comparación de tiempos múltiples. Antes
    de continuar creo que modificaré el esquema de la toma de tiempos: me
    encontŕ un equivalente del GNUProf que en teoría sirve para cosas más
    pequeñas. Si sí funciona, ahora nuestra medida no irá dada en tiempos
    (o no únicamente al menos) sino que irá en ciclos de reloj del
    procesador. De esta manera se es un poco más independiente de la
    computadora en la que se corrn ls pruebas.
    
    De cualquier forma, para los tipos de algoritmos que estamos comparando,
    no creo que tenga sentido seguir haciendo comparaciones de histograma:
    10, 100, 1000, etcétera. Técnicamente son determinísticos, por lo que la
    variación debería ser totalemente lineal.

--- implementaciones/generador_de_reportes.py
+++ implementaciones/generador_de_reportes.py
@@ -11,6 +11,7 @@ se llaman a las respectivas funciones de reportes.
 Proyecto Lovelace.
 """
 
+from json import dumps
 from matplotlib import pyplot
 from numpy import arange
 from subprocess import PIPE
@@ -19,6 +20,11 @@ from subprocess import run
 algoritmos = ['FFX', 'BPS', 'TKR', 'AHR', 'DRBG']
 carpetaGeneral = 'reportes'
 ejecutabeDePruebas = 'binarios/pruebas_desempenio'
+argumentosGenerales = [ejecutabeDePruebas,
+  '127.0.0.1',
+  '3306',
+  'administrador_lovelace_pruebas',
+  'l0v3lac3-padmin']
 
 def generarGraficaTiemposUnitarios(tokenizacion, detokenizacion):
   """
@@ -27,18 +33,18 @@ def generarGraficaTiemposUnitarios(tokenizacion, detokenizacion):
   indices = arange(len(tokenizacion))
   ancho = 0.35
   figura, ejes = pyplot.subplots()
-  barrasTokenizacion = ejes.bar(indices - ancho/2,
+  barrasTokenizacion = ejes.bar(indices - ancho / 2,
     tokenizacion,
     ancho,
     color='#444444',
     label='Tokenización')
-  barrasDetokenizacion = ejes.bar(indices + ancho/2,
+  barrasDetokenizacion = ejes.bar(indices + ancho / 2,
     detokenizacion,
     ancho,
     color='#777777',
     label='Detokenización')
   ejes.set_ylabel('Tiempos (microsegundos)')
-  ejes.yaxis.set_label_coords(-0.12,0.5)
+  ejes.yaxis.set_label_coords(-0.12, 0.5)
   #ejes.set_title('Tiempos unitarios de tokenización y detokenización')
   ejes.set_xticks(indices)
   ejes.set_xticklabels(tuple(algoritmos))
@@ -49,14 +55,14 @@ def generarGraficaTiemposUnitarios(tokenizacion, detokenizacion):
     color='#000000',
     alpha=1.0)
   ejes.tick_params(
-    axis='both',          # changes apply to the x-axis
-    which='both',      # both major and minor ticks are affected
-    bottom=False,      # ticks along the bottom edge are off
-    top=False,         # ticks along the top edge are off
+    axis='both',
+    which='both',
+    bottom=False,
+    top=False,
     left=False,
-    labelbottom=True) # labels along the bottom edge are off
+    labelbottom=True)
   figura.savefig(carpetaGeneral + "/tiempos_unitarios.png",
-    dpi=500)
+    dpi=900)
 
 
 def generarTiemposUnitarios():
@@ -65,14 +71,7 @@ def generarTiemposUnitarios():
   archivo = open(carpetaGeneral + '/tiempos_unitarios.tex', 'w')
   tiemposTokenizacion, tiemposDetokenizacion = (), ()
   for algoritmo in algoritmos:
-    resultado = run([ejecutabeDePruebas,
-      '127.0.0.1',
-      '3306',
-      'administrador_lovelace_pruebas',
-      'l0v3lac3-padmin',
-      algoritmo,
-      '1'],
-      stdout=PIPE)
+    resultado = run(argumentosGenerales + [algoritmo, '1'], stdout=PIPE)
     archivo.write(algoritmo + ' & ')
     for linea in resultado.stdout.decode('UTF-8').splitlines():
       print(linea)
@@ -86,7 +85,34 @@ def generarTiemposUnitarios():
   generarGraficaTiemposUnitarios(tiemposTokenizacion, tiemposDetokenizacion)
 
 
-if __name__ == '__main__':
+def generarGraficaTiemposMultiples():
+  """
+  datos[0] -> Lista con tiempos de tokenización.
+  datos[1] -> Lista con tienmpos de detokenización.
+  datos[0][0] -> Lista con tiempos para el primer algoritmo.
+  datos[0][0][0] -> Lista con tiempo para un número específico de operaciones.
+  """
+  datos = [[], []]
+  for algoritmo in algoritmos:
+    datos[0].append([])
+    datos[1].append([])
+    for i in range(1000, 10001, 1000):
+      resultado = run(argumentosGenerales + [algoritmo, str(i)], stdout=PIPE)
+      for linea in resultado.stdout.decode('UTF-8').splitlines():
+        print(linea)
+        if ' cifrado' in linea:
+          datos[0][len(datos[0]) - 1].append(int(linea.split()[-1]))
+        elif ' descifrado' in linea:
+          datos[1][len(datos[1]) - 1].append(int(linea.split()[-1]))
+  print(datos)
+  reporte = open(carpetaGeneral + '/tiempos_multiples.json', 'w')
+  reporte.write(dumps(datos, indent=2))
 
+
+
+if __name__ == '__main__':
+  """
+  """
   run(['mkdir', '-p', carpetaGeneral])
   generarTiemposUnitarios()
+  generarGraficaTiemposMultiples()
