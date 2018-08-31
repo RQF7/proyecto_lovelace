7290058dd8f28e72ea04a41d57310e44f6e25a6f
Ricardo Quezada <qf7.ricardo@gmail.com>
2018-08-30 21:40:11 -0500

    Cambios a diagrama de modelo de datos
    En la versión anterior me había faltado agregar el import de la clase de
    la llave en el __init__.py, por lo que no salía en el diagrama. Ya está
    arreglado y aquí va la imagen generada por la extensión de django con
    todas las clases.
    
    Lo anterior solo en un ánimo de completitud, pues en realidad ya no se
    está ocupando ese diagrama en el reporte técnico: ahora hay una versión
    de diagrama de clases con tikz.

--- sistema_tokenizador/programa_tokenizador/models/__init__.py
+++ sistema_tokenizador/programa_tokenizador/models/__init__.py
@@ -8,5 +8,6 @@ from .algoritmo import Algoritmo
 from .contador import Contador
 from .estado_de_llave import EstadoDeLlave
 from .estado_de_token import EstadoDeToken
+from .llave import Llave
 from .tipo_de_algoritmo import TipoDeAlgoritmo
 from .token import Token
