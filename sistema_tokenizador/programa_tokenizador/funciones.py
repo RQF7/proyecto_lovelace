386910a965bfd6298e9842473b4f52e2c4b4b904
Laura Borbolla <butyoucancallmecharlie@gmail.com>
2018-09-17 13:37:08 -0500

    Inicios de tokenizar
    Me parece que, una vez que quede este, los demás son lo mismo.
    En este momento pido la contraseña, pero es solo para simplificar
    las pruebas, se debería pedir el sha256 de la contraseña.

--- sistema_tokenizador/programa_tokenizador/funciones.py
+++ sistema_tokenizador/programa_tokenizador/funciones.py
@@ -4,35 +4,109 @@
   Proyecto Lovelace.
 """
 
+import base64
+import hashlib
 from django.core import serializers
 from django.http import HttpResponse
 from django.shortcuts import render
 from json import loads
 from subprocess import PIPE
 from subprocess import run
+import base64
+
+from sistema_tokenizador.general.models.correo import Correo
+from sistema_tokenizador.general.models.usuario import Usuario
 
 from sistema_tokenizador.configuraciones import EJECUTABLE_TOKENIZADOR
 
+def autentificar (peticion, operacion):
+  """
+  Dada una petición, obtiene las credenciales de la cabecera de autenticación
+  y verifica que el usuario sea de tipo cliente y se encuentre en estado
+  <<apobado>> o <<en cambio de llaves>>.
+
+  Ejemplos de pruebas:
+    Usuario no registrado.
+    curl --header "Content-Type: application/json" \
+         --user name:password
+         --request POST \
+         --data '{"pan" : "28045869693113314", "metodo" : "FFX"}' \
+         http://127.0.0.1:8000/programa_tokenizador/tokenizar
+
+    Usuario del tipo incorrecto
+    curl \
+      --user administrador@prueba.com:123456 \
+      --header "Content-Type application/json" \
+      --request POST \
+      --data '{"pan" : "28045869693113314", "metodo" : "FFX"}' \
+      http://127.0.0.1:8000/programa_tokenizador/tokenizar
+
+    Cliente con estado no válido.
+    curl \
+      --user cliente-rechazado@prueba.com:123456 \
+      --header "Content-Type application/json" \
+      --request POST \
+      --data '{"pan" : "28045869693113314", "metodo" : "FFX"}' \
+      http://127.0.0.1:8000/programa_tokenizador/tokenizar
+
+    Cliente cliente con estado válido.
+    curl \
+      --user cliente@prueba.com:123456 \
+      --header "Content-Type application/json" \
+      --request POST \
+      --data '{"pan" : "28045869693113314", "metodo" : "FFX"}' \
+      http://127.0.0.1:8000/programa_tokenizador/tokenizar
+
+    """
+
+    auth_header = str.split(peticion.META['HTTP_AUTHORIZATION'])[1]
+    credenciales = base64.b64decode(auth_header).decode('utf-8').split(':')
+
+    try:
+      usuario = Usuario.objects.get(
+        correo = Correo.objects.get(
+          correo = credenciales[0],
+          contrasenia = hashlib.sha256(
+            credenciales[1].encode('UTF-8')).digest()))
+    except (Usuario.DoesNotExist, Correo.DoesNotExist):
+      return return HttpResponse("401 - credenciales incorrectas")
+
+    if usuario.tipoDeUsuario.nombre != 'cliente':
+      return HttpResponse("403 - usuario no es tipo cliente")
+    elif usuario.estadoDeUsuario.nombre != 'aprobado' and usuario.estadoDeUsuario.nombre != 'en cambio de llaves':
+      return HttpResponse("403 - Todo mal: estado incorrecto")
 
 def tokenizar(peticion):
   """
   Ejecuta la operación de tokenización y regresa el token asignado.
 
-  Ejemplo:
-  curl --header "Content-Type: application/json" \
-       --request POST \
-       --data '{"pan" : "28045869693113314", "metodo" : "FFX"}' \
-       http://127.0.0.1:8000/programa_tokenizador/tokenizar
-
-  Documentación asociada:
-  https://docs.python.org/3.5/library/subprocess.html#subprocess.run
   """
+
+  auth_header = str.split(peticion.META['HTTP_AUTHORIZATION'])[1]
+  credenciales = base64.b64decode(auth_header).decode('utf-8').split(':')
+
+  print('Credenciales:', credenciales)
+  usuario = autentificar(credenciales[0], credenciales[1])
+
+  if usuario != None:
+    if usuario.tipoDeUsuario.nombre != 'cliente':
+      return HttpResponse("403 - usuario no es tipo cliente")
+    elif usuario.estadoDeUsuario.nombre != 'aprobado' and usuario.estadoDeUsuario.nombre != 'en cambio de llaves':
+      return HttpResponse("403 - Todo mal: estado incorrecto")
+  else:
+    return HttpResponse("401 - credenciales incorrectas")
+
   objetoDePeticion = loads(peticion.body)
   pan = objetoDePeticion['pan']
   metodo = objetoDePeticion['metodo']
-  resultado = run([EJECUTABLE_TOKENIZADOR, "-e", metodo, pan, "llave.txt"],
-    stdout=PIPE)
-  return HttpResponse(resultado.stdout)
+
+  return HttpResponse("200 - TODO BIEN")
+
+
+  ##resultado = run([EJECUTABLE_TOKENIZADOR, "-e", metodo, pan, "llave.txt"],
+  ## stdout=PIPE)
+  ##return HttpResponse(resultado.stdout)
+  #return HttpResponse("All is fine")
 
 
 def detokenizar(peticion):
@@ -72,4 +146,3 @@ def ejecutar(peticion):
   """
   resultado = run(peticion.body.split(), stdout=PIPE)
   return HttpResponse(resultado.stdout)
-
