e7c2a216671cadf36ca6cb4762a315c1d9fec414
Laura Borbolla <butyoucancallmecharlie@gmail.com>
2018-10-03 21:10:54 -0500

    Cambios en las gráficas
    Vale, vale; agrego unas gráficas sin título para que no sea taaan
    repetitivo. También ahora la segunda tiene unas leyendas para saber
    visualmente qué significa el color.

--- documentos_entregables/diagramas_comunes/estadisticas_fraudes/estadisticas_fraude.py
+++ documentos_entregables/diagramas_comunes/estadisticas_fraudes/estadisticas_fraude.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 plt.rc('xtick',  labelsize=15)
 plt.rc('ytick',  labelsize=15)
 plt.rc('axes',   labelsize=18)
-plt.rc('axes',   titlesize=21)    
+plt.rc('axes',   titlesize=21)
 '''
 Pérdidas debidas al fraude de comercio en línea en Estados Unidos y Canadá,
 desde 2001 hasta el 2012 en miles de millones de dólares (<<USD billions>>).
@@ -24,8 +24,6 @@ year = [
 plt.plot(year, revenueLost, color='black', linewidth=3)
 plt.ylabel('Pérdidas en miles de millones de dólares')
 plt.xlabel('Año')
-plt.title('Pérdidas por año debidas al fraude en línea (comerciantes en ' +
-  'EE.UU. y Canadá)')
 plt.grid(True)
 plt.show()
 
@@ -48,10 +46,16 @@ cnpLost_rec = [2.1,  2.6,  2.8,  2.9]
 year_rec    = [2011, 2012, 2013, 2014]
 cnpLost_ext = [2.9,  3.1,  3.8,  5.2,  6.4]
 year_ext    = [2014, 2015, 2016, 2017, 2018]
-plt.plot(year_rec, cnpLost_rec, color='black', linewidth=3)
-plt.plot(year_ext, cnpLost_ext, color='grey',  linewidth=3)
+plt.plot(year_rec, cnpLost_rec,
+  color='black',
+  linewidth=3,
+  label='Datos recolectados')
+plt.plot(year_ext, cnpLost_ext,
+  color='grey',
+  linewidth=3,
+  label='Datos estimados')
 plt.ylabel('Pérdidas en miles de millones de dólares')
 plt.xlabel('Año')
-plt.title('Pérdidas debido a fraudes con tarjetas no presentes en EE. UU.')
 plt.grid(True)
+plt.legend()
 plt.show()
