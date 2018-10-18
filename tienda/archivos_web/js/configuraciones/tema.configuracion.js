28bd3d77e27d4236d3f2fea4484309341d216836
Ricardo Quezada <qf7.ricardo@gmail.com>
2018-10-18 15:38:40 -0500

    Cambios en el tema
    Defino desde la configuración del tema (tema.configuracion.js) los
    colosres de fondo. Agrego colores distintos en las tarjetas y el fondo,
    para poder ver contraste. Cambio el color de resaltado. Entre otras
    cosas.

--- tienda/archivos_web/js/configuraciones/tema.configuracion.js
+++ tienda/archivos_web/js/configuraciones/tema.configuracion.js
@@ -2,21 +2,30 @@
  * Configuracipon de colores de aplicación de angular.
  * Tienda en línea.
  * Proyecto Lovelace.
+ *
+ * Catálogo de colores:
+ * https://material.io/design/color/
  */
 
 tienda.config(function($mdThemingProvider) {
   $mdThemingProvider.theme('default')
     .primaryPalette('grey', {
       'default': '200',
-      'hue-1': '400',
-      'hue-2': '500',
-      'hue-3': '600'
+      'hue-1': '200',
+      'hue-2': '200',
+      'hue-3': '200'
     })
-    .accentPalette('red', {
-      'default' : '900'
+    .accentPalette('yellow', {
+      'default' : 'A400'
     })
-    .warnPalette('red', {
-      'default' : '900'
+    .warnPalette('yellow', {
+      'default' : 'A400'
+    })
+    .backgroundPalette('grey',   {
+      'default' : '800',
+      'hue-1': '900',
+      'hue-2': '900',
+      'hue-3': '900'
     })
     .dark();
     $mdThemingProvider.enableBrowserColor();
